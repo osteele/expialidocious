0d27d6c36af57c3996615e17bd2d07f026ea4677
Oliver Steele <steele@osteele.com>
2005-12-27 22:53:33 +0000

    define dataframe class; add left thumb
    git-svn-id: svn+ssh://svn.osteele.com/home/osteele/svn/projects/cloud@14 e7b8d44c-8308-0410-acfb-b763ae30a170

--- analyzer.js
+++ analyzer.js
@@ -1,48 +1,51 @@
-TagList = [];
-TagMap = {};
-TagElements = [];
-Dates = [];
-DateMap = {}; // {Date -> [tagIndex => count]}
-CountArray = [];
 SortedTags = [];
 TagSortInverse = [];
-MaxTag = 0;
 
-var TagData = {
+var DataFrame = function() {
+  this.rowNames = [];
+  this.columnNames = [];
+  this.columns = [];
+};
+
+DataFrame.prototype = {
   processTags: function(data) {
     var posts = data.childNodes;
     var count = 0;
+    var max = 0;
+    var dateMap = {}; // {Date -> [tagIndex => count]}
+    var tagMap = {}
     for (var i in posts) {
       var post = posts[i];
       var date = post.attributes['time'].split('T')[0];
       var tags = post.attributes['tag'].split(' ');
       var di = DateMap[date];
       if (!(di >= 0)) {
-        DateMap[date] = di = Dates.length;
-        Dates.push(date);
-        CountArray.push([]);
+        DateMap[date] = di = this.columnNames.length;
+        this.columnNames.push(date);
+        this.columns.push([]);
       }
-      var counts = CountArray[di];
+      var counts = this.columns[di];
       for (var j in tags) {
         var tag = tags[j];
-        var ti = TagMap[tag];
+        var ti = tagMap[tag];
         if (!(ti >= 0)) {
-          TagMap[tag] = ti = TagList.length;
-          TagList.push(tag);
+          tagMap[tag] = ti = this.rowNames.length;
+          this.rowNames.push(tag);
         }
         while (counts.length <= ti) counts.push(0);
         c = counts[ti] += 1;
-        if (c > MaxTag) MaxTag = c;
+        if (c > max) max = c;
       }
     }
+    this.max = max; // not used
     
-    SortedTags = [].concat(TagList);
+    SortedTags = [].concat(this.rowNames);
     SortedTags.sort();
     TagSortInverse = [];
     for (var i = 0; i < SortedTags.length; i++) {
       var tagname = SortedTags[i];
       var j = 0;
-      while (TagList[j] != tagname) j++;
+      while (this.rowNames[j] != tagname) j++;
       TagSortInverse.push(j);
     }
   },
@@ -50,20 +53,20 @@ var TagData = {
   tagTotals: function (a, b) {
     var tagCounts = [];
     for (var i = a; i < b; i++) {
-      var column = CountArray[i];
+      var column = this.columns[i];
       for (var t = 0; t < column.length; t++) {
         while (t >= tagCounts.length) tagCounts.push(0);
         tagCounts[t] += column[t];
       }
     }
-    while (tagCounts.length < TagList.length) tagCounts.push(0);
+    while (tagCounts.length < this.rowNames.length) tagCounts.push(0);
     return tagCounts;
   },
   
   dateSums: function () {
     var sums = [];
-    for (var i = 0; i < CountArray.length; i++) {
-      var column = CountArray[i];
+    for (var i = 0; i < this.columns.length; i++) {
+      var column = this.columns[i];
       var sum = 0;
       for (var t = 0; t < column.length; t++) {
         sum += column[t];
@@ -75,10 +78,10 @@ var TagData = {
   
   tagsForDate: function (day) {
 		var tags = {};
-		var tagCounts = CountArray[day];
+		var tagCounts = this.columns[day];
 		for (var i = 0; i < tagCounts.length; i++) {
 			if (tagCounts[i])
-				tags[TagList[i]] = tagCounts[i];
+				tags[this.rowNames[i]] = tagCounts[i];
 		}
 		return tags;
 	}
