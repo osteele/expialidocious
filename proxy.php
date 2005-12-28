9498b25a3bb2b3d76eac0ffa7d4f2c4886e794b8
Oliver Steele <steele@osteele.com>
2005-12-28 03:00:13 +0000

    proxy
    git-svn-id: svn+ssh://svn.osteele.com/home/osteele/svn/projects/cloud@18 e7b8d44c-8308-0410-acfb-b763ae30a170

---
+++ proxy.php
@@ -0,0 +1,28 @@
+<?php
+ if (!isset($_SERVER['PHP_AUTH_USER'])) {
+   header('WWW-Authenticate: Basic realm="My Realm"');
+   header('HTTP/1.0 401 Unauthorized');
+   echo 'Text to send if user hits Cancel button';
+   exit;
+ } else {
+	 $user = $_SERVER['PHP_AUTH_USER'];
+	 $passwd = $_SERVER['PHP_AUTH_PW'];
+	 if ($user == 'ows') $user = 'osteele';
+	 //if ($passwd == 'passwd') $passwd = 'pa55wdde';
+	 $url = "http://".$user.":".$passwd."@del.icio.us/api/posts/all";
+	 $file = @fopen($url, "r");
+	 //$file = fsockopen("www.example.com", 80, $errno, $errstr, 30);
+	 //die('err '.$errno.', '.$errstr);
+	 if (!$file) {
+		 die('no file');
+	 }
+	 //die($http_response_header);
+	 header("Content-type: application/xml");
+	 while (!feof($file)) {
+		 $line = fgets($file, 1024);
+		 echo $line;
+	 }
+	 fclose($file);
+ }
+// $http_response_header or stream_get_meta_data()
+?>
