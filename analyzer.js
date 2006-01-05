da4ebe2f3d80b1e0f8cc75e0c59b46bcaac37be8
Oliver Steele <steele@osteele.com>
2006-01-05 23:48:10 +0000

    add version number to cloud.lzx
    git-svn-id: svn+ssh://svn.osteele.com/home/osteele/svn/projects/cloud@32 e7b8d44c-8308-0410-acfb-b763ae30a170

--- analyzer.js
+++ analyzer.js
@@ -1,7 +1,8 @@
 /*
 Copryight 2005-2006 Oliver steele.  Some rights reserved.
 
-This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License: http://creativecommons.org/licenses/by-nc-sa/2.5/.
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License:
+http://creativecommons.org/licenses/by-nc-sa/2.5/.
 */
 
 var DataFrame = function () {
